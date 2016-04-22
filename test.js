e4c5e478c5baad8a53c5b70cff1570ecf96ddd9b
Kevin Martensson <kevinmartensson@gmail.com>
2016-04-22 13:54:02 +0200

    Use AVA in tests

--- test.js
+++ test.js
@@ -1,10 +1,6 @@
-'use strict';
-var assert = require('assert');
-var defaultBrowser = require('./');
+import test from 'ava';
+import m from './';
 
-it('should return the default browser', function () {
-	return defaultBrowser().then(function (id) {
-		console.log('Bundle id:', id);
-		assert(/^com\./.test(id));
-	});
+test(async t => {
+	t.regex(await m(), /^com\./);
 });
